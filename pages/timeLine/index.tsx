8b4f5b75be6b632840a3930d9527822639f7f48e
折秋 <1533424140@qq.com>
2023-01-14 17:48:40 +0800

    feat: 打包测试

--- pages/timeLine/index.tsx
+++ pages/timeLine/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import ProcessLine from '../../components/process-line'
-import styles from './index.module.scss'
+import styles from '../../styles/timeline.module.scss'
 
 export default function Timeline() {
     return (
