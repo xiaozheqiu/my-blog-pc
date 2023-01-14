c45225de354d7d630343ee00c1292a4103a365e4
折秋 <1533424140@qq.com>
2023-01-14 17:36:57 +0800

    feat: 修复打包报错问题

--- pages/timeLine/index.tsx
+++ pages/timeLine/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import ProcessLine from '../../components/process-line'
-import styles from '../../styles/timeline.module.scss'
+import styles from './index.module.scss'
 
 export default function Timeline() {
     return (
