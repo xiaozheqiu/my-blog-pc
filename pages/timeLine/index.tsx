6aa8fb1167001eb48440c51e15b0a1454fad5a1f
折秋 <1533424140@qq.com>
2023-01-14 17:43:36 +0800

    feat: 打包测试

--- pages/timeLine/index.tsx
+++ pages/timeLine/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import ProcessLine from '../../components/process-line'
+import styles from './index.module.scss'
 
 export default function Timeline() {
     return (
         <Layout>
-            <ProcessLine />
+            <div className={styles.timeLine}>
+                <ProcessLine />
+            </div>
         </Layout>
     )
 }
