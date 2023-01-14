f2027c78eb155999fc20eab9aea93681a5920bb0
折秋 <1533424140@qq.com>
2023-01-14 17:32:46 +0800

    feat: 修复打包报错问题

--- pages/timeLine/index.tsx
+++ pages/timeLine/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Layout from "../../components/layout";
-import ProcessLine from "../../components/process-line";
-import styles from "../../styles/timeline.module.scss";
+import React from 'react'
+import Layout from '../../components/layout'
+import ProcessLine from '../../components/process-line'
+import styles from '../../styles/timeline.module.scss'
 
 export default function Timeline() {
-  return (
-    <Layout>
-      <div className={styles.timeLine}>
-        <ProcessLine />
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div className={styles.timeLine}>
+                <ProcessLine />
+            </div>
+        </Layout>
+    )
 }
