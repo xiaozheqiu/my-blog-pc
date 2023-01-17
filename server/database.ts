e232890e7ca95250131c0bb8767dcaaff2457d09
折秋 <1533424140@qq.com>
2023-01-17 22:44:38 +0800

    feat: 修改serverless函数调用错误的问题

--- server/database.ts
+++ server/database.ts
@@ -7,6 +7,7 @@ const { MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PWD, MYSQL_DB } = config
 const sequelize = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
     host: MYSQL_HOST,
     dialect: 'mysql',
+    dialectModule: require('mysql2'),
     port: MYSQL_PORT,
     timezone: '+08:00',
     logging: (msg) => Console.debug(msg)
